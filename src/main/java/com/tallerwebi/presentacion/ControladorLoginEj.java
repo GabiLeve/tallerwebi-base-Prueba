37f0fbe1bfc225bf4b7c44248d620550fa340588
Gabriela <leverattomariag@gmail.com>
2025-05-03 11:14:03 -0300

    agregado de fragmentos con thymeleaf y redireccionamiento

--- src/main/java/com/tallerwebi/presentacion/ControladorLoginEj.java
+++ src/main/java/com/tallerwebi/presentacion/ControladorLoginEj.java
@@ -9,9 +9,18 @@ public class ControladorLoginEj {
 
     //asocio una URL a un métodoo de Java
     // @RequestMapping le dice a Spring MVC, Cuando llegue una petición HTTP a tal URL (en este caso la URL terminada en /verVistaLogin), quiero que ejecutes este métodoo
-    @RequestMapping("/verVistaLogin")
+    @RequestMapping("/verVistaLogin") //esto se escrib en el navegador
     public ModelAndView mostrarLogin(){
 
         return new ModelAndView("vistaLogin"); //se crea un objeto ModelAndView con el nombre "vistaLogin". Aca spring se encarga de armar la ruta a la vista. Configurado en SpringWebConfirg.java
+        //"vistaLogin.html" hay que crear en WEB-INF/thymeleaf
     }
+
+    // esto iria en ControladorArtista
+    @RequestMapping("/verCanciones")
+    public ModelAndView mostrarCanciones(){
+
+        return new ModelAndView("canciones");
+    }
+
 }
